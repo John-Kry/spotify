7c5f65f0857d7f6a0d494b6c878449eae9fd04c9
John Kryspin <johnkryspin@Johns-MBP.home>
2019-08-18 22:35:08 -0400

    redirect to correct location

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import accessTokenStore from './accessTokenStore'
 import axios from 'axios'
-const spotifyLoginLink = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=http:%2F%2Flocalhost:3000%2FauthenticationCallback&scope=user-read-private%20user-read-email%20user-top-read%20user-modify-playback-state&response_type=token&state=123`
+console.log(window.location.href)
+var arr = window.location.href.split("/");
+console.log(arr)
+const spotifyLoginLink = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=http:%2F%2F${arr[2]}%2FauthenticationCallback&scope=user-read-private%20user-read-email%20user-top-read%20user-modify-playback-state&response_type=token&state=123`
 function Root(props) {
     let [topTracks, setTopTracks] = useState([])
     useEffect(() => {
