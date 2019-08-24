a5a8b307e75bb06a98ac30f35a370b0acb6748bb
John Kryspin <johnkryspin@gmail.com>
2019-08-24 11:36:51 -0400

    add state to show which values are displaying

--- src/Root.js
+++ src/Root.js
@@ -5,26 +5,35 @@ import bulma from 'bulma'
 var arr = window.location.href.split("/");
 const spotifyLoginLink = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=http:%2F%2F${arr[2]}%2FauthenticationCallback&scope=user-read-private%20user-read-email%20user-top-read%20user-modify-playback-state&response_type=token&state=123`
 const durationMap = {
-    "week": "short_term",
-    "month": "medium_term",
-    "year": "long_term"
+    "week": {
+        displayValue: "Week",
+        value: "short_term"
+    },
+    "month": {
+        displayValue: "Month",
+        value: "medium_term"
+    },
+    "year": {
+        displayValue: "Year",
+        value: "long_term"
+    }
 }
 function Root(props) {
     let [topTracks, setTopTracks] = useState([])
-    let [duration, setDuration] = useState("short_term")
+    let [duration, setDuration] = useState("week")
     if (!accessTokenStore.getAccessToken())
         window.location = spotifyLoginLink
     useEffect(() => {
         updateTracks()
     }, [duration]);
     function updateTracks() {
-        axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${duration}&limit=50`, { headers: { 'Authorization': "Bearer " + accessTokenStore.getAccessToken() } })
+        axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${durationMap[duration].value}&limit=50`, { headers: { 'Authorization': "Bearer " + accessTokenStore.getAccessToken() } })
             .then((result) => {
                 setTopTracks(result.data.items)
             })
     }
     function handleDurationClick(e) {
-        setDuration(durationMap[e.target.name])
+        setDuration(e.target.name)
     }
     return (
         <div className="App">
@@ -32,20 +41,25 @@ function Root(props) {
                 <div>
                     <h1 className="header-title">Your Top <i>Spotify</i> Tracks</h1>
                 </div>
-                <div className="columns ">
+                <div className="columns">
                     <div className="column">
                         <button className={"header-button"} name="week" onClick={handleDurationClick}>Past week</button>
                         <button className="header-button" name="month" onClick={handleDurationClick}>Past month</button>
                         <button className="header-button" name="year" onClick={handleDurationClick}>Past year</button>
                     </div>
                 </div>
+                <div className="columns">
+                    <div className="column">
+                        <h3 className="header-selected">Currently: {durationMap[duration].displayValue}</h3>
+                    </div>
+                </div>
             </div>
-            {topTracks.map((track ,index) => {
+            {topTracks.map((track, index) => {
                 return <div className="columns" key={index}>
                     <div className="column">
                         <span id="title" onClick={() => {
                             axios.put("https://api.spotify.com/v1/me/player/play", { uris: [track.uri] }, { headers: { 'Authorization': "Bearer " + accessTokenStore.getAccessToken() } })
-                        }}>{index +1}. {track.name}</span> by <span id="artist">{track.artists[0].name}</span>
+                        }}>{index + 1}. {track.name}</span> by <span id="artist">{track.artists[0].name}</span>
                     </div>
                 </div>
             })}
