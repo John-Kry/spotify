db3ba819ef3619d05d15f0652430d63d7170500c
John Kryspin <johnkryspin@gmail.com>
2019-08-24 11:26:50 -0400

    added multiple options for your top songs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import React , {useEffect}from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Root from './Root';
 import AutheticationCallback from './AuthenticationCallback';
 
 function App(props) {
-  useEffect(() => {
-    console.log(props)
-  });
   return (
     <Router>
         <Route path="/" exact component={Root} />
--- src/Root.js
+++ src/Root.js
@@ -4,29 +4,51 @@ import axios from 'axios'
 import bulma from 'bulma'
 var arr = window.location.href.split("/");
 const spotifyLoginLink = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=http:%2F%2F${arr[2]}%2FauthenticationCallback&scope=user-read-private%20user-read-email%20user-top-read%20user-modify-playback-state&response_type=token&state=123`
+const durationMap = {
+    "week": "short_term",
+    "month": "medium_term",
+    "year": "long_term"
+}
 function Root(props) {
     let [topTracks, setTopTracks] = useState([])
+    let [duration, setDuration] = useState("short_term")
+    if (!accessTokenStore.getAccessToken())
+        window.location = spotifyLoginLink
     useEffect(() => {
-        if (!accessTokenStore.getAccessToken())
-            window.location = spotifyLoginLink
-        if (accessTokenStore.getAccessToken() && topTracks.length === 0) {
-            axios.get('https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50', { headers: { 'Authorization': "Bearer " + accessTokenStore.getAccessToken() } })
-                .then((result) => {
-                    setTopTracks(result.data.items)
-                })
-        }
-    });
+        updateTracks()
+    }, [duration]);
+    function updateTracks() {
+        axios.get(`https://api.spotify.com/v1/me/top/tracks?time_range=${duration}&limit=50`, { headers: { 'Authorization': "Bearer " + accessTokenStore.getAccessToken() } })
+            .then((result) => {
+                setTopTracks(result.data.items)
+            })
+    }
+    function handleDurationClick(e) {
+        setDuration(durationMap[e.target.name])
+    }
     return (
         <div className="App">
-                {topTracks.map((track) => {
-                    return <div class="columns">
-                        <div class="column">
-                           <span id="title"onClick={() => {
-                                    axios.put("https://api.spotify.com/v1/me/player/play", { uris: [track.uri] }, { headers: { 'Authorization': "Bearer " + accessTokenStore.getAccessToken() } })
-                                }}>{track.name}</span> by <span id="artist">{track.artists[0].name}</span>  
-                           </div>
+            <div className="header">
+                <div>
+                    <h1 className="header-title">Your Top <i>Spotify</i> Tracks</h1>
+                </div>
+                <div className="columns ">
+                    <div className="column">
+                        <button className={"header-button"} name="week" onClick={handleDurationClick}>Past week</button>
+                        <button className="header-button" name="month" onClick={handleDurationClick}>Past month</button>
+                        <button className="header-button" name="year" onClick={handleDurationClick}>Past year</button>
                     </div>
-                })}
-            </div>)
-    }
+                </div>
+            </div>
+            {topTracks.map((track ,index) => {
+                return <div className="columns" key={index}>
+                    <div className="column">
+                        <span id="title" onClick={() => {
+                            axios.put("https://api.spotify.com/v1/me/player/play", { uris: [track.uri] }, { headers: { 'Authorization': "Bearer " + accessTokenStore.getAccessToken() } })
+                        }}>{index +1}. {track.name}</span> by <span id="artist">{track.artists[0].name}</span>
+                    </div>
+                </div>
+            })}
+        </div>)
+}
 export default Root
